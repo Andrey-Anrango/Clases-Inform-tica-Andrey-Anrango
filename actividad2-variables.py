2368d98f03a173389876c87bccced1e0ef190763
Andrey Anrango <e11675@ism.edu.ec>
2026-04-15 08:20:47 -0500

    act2cambio

---
+++ actividad2-variables.py
@@ -0,0 +1,30 @@
+# %%
+
+first_name = "Andrey"
+longitud = len(first_name)
+print("first_name length: ", len(first_name))
+print(type(first_name))
+print(type(longitud))
+
+# %%
+primer_nombre, apellido, pais, edad, estado civil = "Andrey". "Anrango", "Ecuador", "16", "soltero"
+# %%
+print(primer_nombre)
+# %%
+print(apellido)
+# %%
+print(pais)
+# %%
+print(edad)
+# %%
+print(estado civil)
+# %%
+"""Andrey Anrango
+Actividad 2: Nivel 1 y 2
+Hoy es 15/4/2026"""
+# %%
+nombre = "Andrey"
+# %%
+apelido= "Anrango"
+# %%
+nombreCompleto=nombre + " " +apellido
