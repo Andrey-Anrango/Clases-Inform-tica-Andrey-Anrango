255d42a28af93cb4d22852e960949fc368ce3ce0
Andrey Anrango <e11675@ism.edu.ec>
2026-04-15 08:54:11 -0500

    ACT2-CAMBIOS-RECIENTES-SEGUIR-ENCASA

--- Act2(variables).py
+++ Act2(variables).py
@@ -23,4 +23,48 @@ print(pais)
 print(edad)
 # %%
 print(estado civil)
-
+# %%
+"""Andrey Anrango
+Actividad 2: Nivel 1 y 2
+Hoy es 15/4/2026"""
+# %%
+nombre = "Andrey"
+# %%
+apelido= "Anrango"
+# %%
+nombreCompleto=nombre + " " +apellido
+# %%
+pais = "Ecuador"
+# %%
+ciudad = "Quito"
+# %%
+edad = "16"
+# %%
+año = "2026"
+# %%
+estadoCivil = "soltero"
+# %%
+esVerdadero = "True"
+# %%
+luzEncendida = "False"
+# %%
+nombre, apellido, pais, ciudad = "Andrey", "Anrango", "Ecuador", "Quito"
+# %%
+"""Nivel 2  """
+# %%
+type(nombre), type(apellido), type(edad), type(esVerdadero), type(luzEncendida), type(nombreCompleto), type(año), type(estadoCivil), type(ciudad), type(pais)
+# %%
+nombre = "Andrey"
+print("longitud del nombre: ", len(nombre))
+# %%
+nombre = "Andrey"
+apellido = "Anrango"
+longitudN = len(nombre)
+longitudA = len(apellido)
+if longitudN > longitudA:
+    print("El nombre es más largo que el apellido")
+elif longitudN < longitudA:
+    print("El apellido es más largo que el nombre")
+else:
+    print("El nombre y el apellido tienen la misma longitud")
+# %%
