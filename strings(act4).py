b1cfea968c203f2e09edab7c15742e415daf41d1
Andrey Anrango <e11675@ism.edu.ec>
2026-04-21 12:36:16 -0500

    cambios en strings

--- strings(act4).py
+++ strings(act4).py
@@ -20,3 +20,17 @@ print("python\nchallenge")
 print("nombre\tSemana1\tSemana2\tSemana3")
 print("Simbolo(\\)")
 print(f"Mi nombre es: {nombre} y mi apellido es {apellido}")
+# %%
+palabra="Python"
+a,b,c,d,e,f=palabra
+print(palabra[2])
+last_three=palabra[3:6]
+print("Las últimas tres letras de la palabra 'Python' son:", last_three)
+# %%
+greeting = "Hello, World!"
+print(greeting[::-1])
+thn=palabra[2:3:6]
+print(palabra[0:6:2])
+challenge="thirty days of python" 
+print(challenge.capitalize())
+print(challenge.count("y"))
