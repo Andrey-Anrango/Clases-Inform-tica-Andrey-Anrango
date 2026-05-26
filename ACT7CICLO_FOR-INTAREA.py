ba2b9efe947cf90a83d72a7989eb8c47e7213c9f
Andrey Anrango <e11675@ism.edu.ec>
2026-05-26 12:36:10 -0500

    cambios act9 con fragmentacion

--- ACT7CICLO_FOR-INTAREA.py
+++ ACT7CICLO_FOR-INTAREA.py
@@ -75,3 +75,47 @@ while True:
     else:
         promedio = suma / 5
     print(f"El promedio es: {promedio}")
+print("Fin del programa")
+
+num= int(input("Ingrese un número para hallar su tabla de multiplicación: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+print("Tabla de multiplicación completa.")
+"""---------"""
+notas = [5, 8, 9, 7, 10]
+cantidad=0
+suma=0
+for i in range(1, 6):
+    suma=suma+notas[i]
+    cantidad= cantidad+1
+promedio =suma/cantidad
+print(f"El promedio de las notas es: {promedio}")
+"""---------"""
+
+# %%
+# Programa para generar una tabla de multiplicar
+# usando solo los factores pares: 2, 4, 6, 8 y 10
+numero = int(input("Ingrese un número: "))
+for i in range (2, 11, 2):
+    resultado= numero*i
+    print(f"{numero}x{i} = {resultado}")
+# %%
+numero = int(input("Ingrese un número: "))
+for i in range (9, 0, -2):
+    resultado= numero*i
+    print(f"{numero}x{i} = {resultado}")
+# %%
+# Asignar estudiantes a los puestos de un laboratorio
+# El laboratorio tiene 3 filas y 4 computadoras por cada fila
+# Ciclo externo: recorre las filas del laboratorio
+# range(1, 4) genera los valores 1, 2 y 3
+
+nombre = ["Ana", "Luis", "María", "Carlos", "Sofía", "Mateo", "Daniela", "Pedro", "Valeria", "José", "Camila", "Andrés"]
+indice = 0  
+for fila in range(1, 4):
+    # Recorrer computadoras
+    for computadora in range(1, 5):
+        print(f"{nombre[indice]} asignado a Fila {fila} - Computadora {computadora}")
+        indice += 1
+    print(f"Fin de la fila: {fila}")
+print("Fin del programa")
--- actividad9-funciones.py
+++ actividad9-funciones.py
@@ -6,3 +6,79 @@ def generate_full_name():
 generate_full_name()
 print("----")
 generate_full_name()
+
+
+# EJEMPLO 1: Registro
+def mostrar_instrucciones():
+    print("=== INSTRUCCIONES DEL PROGRAMA ===")
+    print("1. Ingresa tu nombre.")
+    print("2. Ingresa tu edad.")
+    print("3. El programa mostrará un mensaje personalizado.")
+def mostrar_despedida():
+    print("Gracias por usar el programa.")
+
+print("=== SISTEMA DE REGISTRO ===")
+opcion = input('¿Deseas ver las instrucciones? si/no: ')
+if opcion == 'si':
+    mostrar_instrucciones()
+nombre = input('Ingrese su nombre: ')
+edad = input('Ingrese su edad: ')
+print(f'Hola {nombre} tienes {edad} años.')
+mostrar_despedida()
+
+
+# EJEMPLO 2: Suma
+def mostrar_instrucciones():
+    print("=== INSTRUCCIONES ===")
+    print("Debe ingresar dos números.")
+    print("El programa sumará esos números.")
+    print("Puede escribir ayuda si no entiende qué hacer.")
+
+print("=== SUMA DE DOS NÚMEROS ===")
+mostrar_instrucciones()
+dato = input('Ingrese el primer número o escriba ayuda: ')
+if dato == 'ayuda':
+    mostrar_instrucciones()
+    dato = input('Ingrese el primer número: ')
+numero1 = int(dato)
+numero2 = int(input('Ingrese el segundo número: '))
+suma = numero1 + numero2
+print(f'La suma es: {suma}')
+
+#con parametros
+def saludar(nombre):
+    print(f"Hola {nombre}, bienvenido al curso de Python.")
+saludar("Andrey")
+
+
+def mostrar_estudiante(nombre, curso):
+    print("Estudiante registrado:")
+    print(f"Nombre: {nombre}")
+    print(f"Curso: {curso}")
+    print("----------------------")
+
+
+# Función sin parámetros
+def mensaje_final():
+    print("Fin del programa")
+
+
+# Preguntar cuántos estudiantes se van a ingresar
+cantidad = int(input("¿Cuántos estudiantes desea ingresar? "))
+
+contador = 0
+
+while contador < cantidad:
+    print(f"\nEstudiante {contador + 1}")
+    
+    nombre = input("Ingrese el nombre: ")
+    curso = input("Ingrese el curso: ")
+    
+    # Llamar a la función
+    mostrar_estudiante(nombre, curso)
+    
+    contador += 1
+
+# Mensaje final
+mensaje_final()
+
